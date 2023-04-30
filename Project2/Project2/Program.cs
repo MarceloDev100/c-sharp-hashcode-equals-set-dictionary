25fb11ee173d10975815b74fc9e92fa38bd66888
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-29 22:02:21 -0300

    Use of SortedSet class

--- Project2/Project2/Program.cs
+++ Project2/Project2/Program.cs
@@ -4,18 +4,33 @@
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
+            SortedSet<int> a = new SortedSet<int>() { 10, 0, 2, 4, 5, 6, 8 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
+            // union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
 
-            Console.WriteLine(set.Contains("Computer"));
+            // intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
 
-            foreach (string p in set)
+            // difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach(T obj in collection)
             {
-                Console.WriteLine(p);
+                Console.Write(obj + " ");
             }
+            Console.WriteLine();
         }
     }
 }
